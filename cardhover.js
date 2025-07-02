19fbbfee60c4b6eee4f178fa05b78ad345e888f2
ZeeshanAhmedPatel <zeeshanahmedpatel1@gmail.com>
2025-07-02 22:34:15 +0500

    Added all project files

---
+++ cardhover.js
@@ -0,0 +1,162 @@
+$(document).ready(function(){
+    // for 1st card
+    $('#img1').hover(function()
+    {
+    $('#img1').attr('src','Card Hover Imgs/card1img.jpg');
+    },
+    function()
+    {
+    $('#img1').attr('src','Wall Tiles/bathroomwall.jpg');
+    })
+    // for2ndcard
+    $('#img2').hover(function()
+    {
+    $('#img2').attr('src','Card Hover Imgs/card2img.jpg');
+    },
+    function()
+    {
+    $('#img2').attr('src','Wall Tiles/kitchenwall.jpg');
+    })
+    // for3rdcard
+    $('#img3').hover(function()
+    {
+    $('#img3').attr('src','Card Hover Imgs/cardimg3.jpg');
+    },
+    function()
+    {
+    $('#img3').attr('src','Wall Tiles/livingroomwall.jpg');
+    })    
+    // for4thcard
+    $('#img4').hover(function()
+    {
+    $('#img4').attr('src','Card Hover Imgs/cardimg4.jpg');
+    },
+    function()
+    {
+    $('#img4').attr('src','Wall Tiles/roomwall.jpg');
+       
+    })  
+    // for5thcard
+    $('#img5').hover(function()
+    {
+    $('#img5').attr('src','Card Hover Imgs/cardimg5.jpg');
+    },
+    function()
+    {
+    $('#img5').attr('src','Wall Tiles/commercialwall.jpg');
+    }) 
+        
+    
+    // for6thcard
+    $('#img6').hover(function()
+    {
+    $('#img6').attr('src','Card Hover Imgs/cardimg6.jpg');
+    },
+    function()
+    {
+    $('#img6').attr('src','Wall Tiles/outdoorwall.jpg');
+    }) 
+       
+    // floor tiles
+    
+    // for 7th card
+    $('#img7').hover(function()
+    {
+    $('#img7').attr('src','Card Hover Imgs/cardimg8.jpg');
+    },
+    function()
+    {
+    $('#img7').attr('src','Floor Tiles/bathroomfloor.jpg');
+    })
+    // for8thcard
+    $('#img8').hover(function()
+    {
+    $('#img8').attr('src','Card Hover Imgs/cardimg7.jpg');
+    },
+    function()
+    {
+    $('#img8').attr('src','Floor Tiles/kitchenfloor.jpg');
+    })
+    // for9thcard
+    $('#img9').hover(function()
+    {
+    $('#img9').attr('src','Card Hover Imgs/cardimg9.jpg');
+    },
+    function()
+    {
+    $('#img9').attr('src','Floor Tiles/livingroomfloor.jpg');
+    })    
+    // for10thcard
+    $('#img10').hover(function()
+    {
+    $('#img10').attr('src','Card Hover Imgs/cardimg12.jpg');
+    },
+    function()
+    {
+    $('#img10').attr('src','Floor Tiles/bedroomfloor.jpg');
+       
+    })  
+    // for11thcard
+    $('#img11').hover(function()
+    {
+    $('#img11').attr('src','Card Hover Imgs/cardimg11.jpg');
+    },
+    function()
+    {
+    $('#img11').attr('src','Floor Tiles/commercialfloor.jpg');
+    }) 
+        
+    
+    // for12thcard
+    $('#img12').hover(function()
+    {
+    $('#img12').attr('src','Card Hover Imgs/cardimg10.jpg');
+    },
+    function()
+    {
+    $('#img12').attr('src','Floor Tiles/outdoorfloor.jpg');
+    }) 
+    
+    // special tiles
+    // for13thcard
+    $('#img13').hover(function()
+    {
+    $('#img13').attr('src','Card Hover Imgs/spcard1.jpg');
+    },
+    function()
+    {
+    $('#img13').attr('src','Special Tiles/germfreetile.jpg');
+       
+    })  
+    // for14thcard
+    $('#img14').hover(function()
+    {
+    $('#img14').attr('src','Card Hover Imgs/spcard2.jpg');
+    },
+    function()
+    {
+    $('#img14').attr('src','Special Tiles/tactile.jpg');
+    }) 
+        
+    
+    // for15thcard
+    $('#img15').hover(function()
+    {
+    $('#img15').attr('src','Card Hover Imgs/spcard3.webp');
+    },
+    function()
+    {
+    $('#img15').attr('src','Special Tiles/antistatictile.jpg');
+    }) 
+    
+    // for16thcard
+    $('#img16').hover(function()
+    {
+    $('#img16').attr('src','Card Hover Imgs/spcard4.jpg');
+    },
+    function()
+    {
+    $('#img16').attr('src','Special Tiles/coolrooftile.jpg');
+    }) 
+        }) 
+    //    end brackets
